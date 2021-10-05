c9bf706201b8e881363b670285164080e4a8c10c
devalone <devalone@126.com>
2021-10-05 17:49:54 +0800

    修改子网为 192.168.0.0/255.255.255

--- code/environments/develop/manifests/site.pp
+++ code/environments/develop/manifests/site.pp
@@ -1,24 +1,24 @@
 node default {
   notify { 'default node':
-    message => "not match your node, this is default node",
+    message => 'not match your node, this is default node',
   }
 }
 
 include localtime
 
 class { 'ntp':
-  servers    => ['192.168.10.2 prefer','cn.ntp.org.cn'],
-  restrict   => [
+  servers  => ['192.168.0.2 prefer','cn.ntp.org.cn'],
+  restrict => [
     '-4 default notrap nomodify nopeer noquery',
     '-6 default notrap nomodify nopeer noquery',
     '127.0.0.1',
     '-6 ::1',
 
     #允许这个网段的对时请求
-    '192.168.10.0 mask 255.255.255.0 nomodify',
+    '192.168.0.0 mask 255.255.255.0 nomodify',
 
     #允许上层ntp server的所有权限
-    '192.168.10.2'
+    '192.168.0.2'
   ],
 }
 
--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
   notify { 'default node':
-    message => "[production] environement, not match your node, this is default node.",
+    message => '[production] environement, not match your node, this is default node.',
   }
 }
 
@@ -9,14 +9,14 @@ include localtime
 node 'dnsmasq.sansovo.org' {
   class { 'ntp':
     servers    => ['ntp.aliyun.com','cn.ntp.org.cn','127.127.1.0'],
-    interfaces => ['127.0.0.1', '192.168.10.2'],
+    interfaces => ['127.0.0.1', '192.168.0.2'],
     restrict   => [
       '-4 default notrap nomodify nopeer noquery',
       '-6 default notrap nomodify nopeer noquery',
       '127.0.0.1',
       '-6 ::1',
       #允许这个网段的对时请求
-      '192.168.10.0 mask 255.255.255.0 nomodify',
+      '192.168.0.0 mask 255.255.255.0 nomodify',
 
       #允许上层ntp server的所有权限      
       'ntp.aliyun.com',
@@ -27,7 +27,7 @@ node 'dnsmasq.sansovo.org' {
 
 node /\w+\.sansovo\.org$/ {
   class { 'ntp':
-    servers  => ['192.168.10.2 prefer','cn.ntp.org.cn'],
+    servers  => ['192.168.0.2 prefer','cn.ntp.org.cn'],
     restrict => [
       '-4 default notrap nomodify nopeer noquery',
       '-6 default notrap nomodify nopeer noquery',
@@ -35,10 +35,10 @@ node /\w+\.sansovo\.org$/ {
       '-6 ::1',
 
       #允许这个网段的对时请求
-      '192.168.10.0 mask 255.255.255.0 nomodify',
+      '192.168.0.0 mask 255.255.255.0 nomodify',
 
       #允许上层ntp server的所有权限
-      '192.168.10.2'
+      '192.168.0.2'
     ],
   }
 }
